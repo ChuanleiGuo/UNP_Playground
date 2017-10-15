b819bc12a67708151986d045d0d98d6b2c7e0d05
ChuanleiGuo <chuanleiguo@gmail.com>
2017-10-15 14:14:03 +0800

    deal with SIGCHLD

---
+++ tcpcliserv/tcpserv02.c
@@ -0,0 +1,28 @@
+#include "unp.h"
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    pid_t child_pid;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+    void sig_chld(int);
+    
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+    Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+    Listen(listenfd, LISTENQ);
+    Signal(SIGCHLD, sig_chld);
+    for ( ; ; ) {
+        clilen = sizeof(cliaddr);
+        connfd = Accept(listenfd, (SA *)&cliaddr, &clilen);
+        if ((child_pid = Fork()) == 0) {
+            Close(listenfd);
+            str_echo(connfd);
+            exit(0);
+        }
+        Close(connfd);
+    }
+}
